ed4cc054b207a85c867f1306b41d7b7cf3550884
fredd <freddjame@gmail.com>
2023-05-08 19:55:21 +0300

    sixth commit

---
+++ src/test/java/com/workers/workers/WorkersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workers.workers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
